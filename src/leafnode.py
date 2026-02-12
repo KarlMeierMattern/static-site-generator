a07ebf735780a99daa25c4450d125bcb4aa50a2f
KarlMeierMattern <karlmeiermattern@gmail.com>
2026-02-12 09:58:48 +0200

    Added: leaf node

---
+++ src/leafnode.py
@@ -0,0 +1,18 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+
+        if self.value is None:
+            raise ValueError("Leaf node must have a value")
+
+        if self.tag is None:
+            return self.value
+        
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
--- src/textnode.py
+++ src/textnode.py
@@ -1,14 +1,5 @@
 from enum import Enum
 
-"""
-text (plain)
-**Bold text**
-_Italic text_
-`Code text`
-Links, in this format: [anchor text](url)
-Images, in this format: ![alt text](url)
-"""
-
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -22,12 +13,6 @@ class TextNode:
         self.text = text
         self.text_type = text_type
         self.url = url
-
-    """
-    Create an __eq__ method on TextNode that compares self to another TextNode instance (other) and returns True if all of their properties are equal. Our future unit tests will rely on this method to compare objects.
-    Create a __repr__ method that returns a string representation of the TextNode object. It should look like this:
-    "TextNode(text='Hello, World!', text_type='text', url=None)"
-    """
 
     def __eq__(self, other):
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
